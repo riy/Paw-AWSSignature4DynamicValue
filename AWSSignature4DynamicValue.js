riy/Paw-AWSSignature4DynamicValue

commit 47f53240d7e1d4607b614a4ab6027db5473a03e9

Add code to include arbitrary extra `x-amz-` headers

AWSSignature4DynamicValue.js
@@ -196,14 +196,28 @@ var AWSSignature4DynamicValue = function() {
             daytime = day + 'T' + amzTime(now)
         }
         var bodyHash = hash256(request.body || '')
-
-        // Step 1
+        
+        // Search for other signed headers to include. We will assume any headers that begin with X-Amz-<*> will be included
         var signedHeaders = 'host;x-amz-date'
+        var headers = '' // The actual headers to sign
+        var names = request.getHeaderNames()
+        if (names) {
+            names.forEach(function(name) {
+                var lower = name.toLowerCase()
+                if (lower !== 'x-amz-date' && lower.startsWith('x-amz-') {
+                    signedHeaders += ';'+lower
+                    headers += lower + ':' + request.getHeaderByName(name, false) + '\n'
+                }
+            })
+        }
+        
+        // Step 1
         var canonical = request.method + '\n' +
             uri.pathname + '\n' +
             getParametersString(request, uri.search) + '\n' +
             'host:' + uri.hostname.toLowerCase() + '\n' +
             'x-amz-date:' + daytime + '\n' +
+            headers +
             '\n' +
             signedHeaders + '\n' +
             bodyHash

commit db5f51d3f4e40814f74a5e46d61e23154606b3d8

Fix syntax error
this was introduced in https://github.com/badslug/Paw-AWSSignature4DynamicValue/pull/6/commits/47f53240d7e1d4607b614a4ab6027db5473a03e9

AWSSignature4DynamicValue.js
@@ -211,7 +211,7 @@ var AWSSignature4DynamicValue = function() {
         if (names) {
             names.forEach(function(name) {
                 var lower = name.toLowerCase()
-                if (lower !== 'x-amz-date' && lower.startsWith('x-amz-') {
+                if (lower !== 'x-amz-date' && lower.startsWith('x-amz-')) {
                     signedHeaders += ';'+lower
                     headers += lower + ':' + request.getHeaderByName(name, false) + '\n'
                 }